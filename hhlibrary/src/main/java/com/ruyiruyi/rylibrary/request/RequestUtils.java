dcdb4a1942049da6231c948ecf177700700b0b42
dongbin <qc.dancer@qq.com>
2025-08-27 11:49:53 +0800

    新疆七师 -172.16.200.32  8011   新疆七师内网

--- hhlibrary/src/main/java/com/ruyiruyi/rylibrary/request/RequestUtils.java
+++ hhlibrary/src/main/java/com/ruyiruyi/rylibrary/request/RequestUtils.java
@@ -6,7 +6,7 @@ package com.ruyiruyi.rylibrary.request;
 
 public class RequestUtils {
 //    public static String REQUEST__URL_HLJ = "http://220.170.165.1:8011/";//release
-    public static String REQUEST__URL_HLJ = "http://172.16.200.32:8444/";//release 新疆七师内网
+    public static String REQUEST__URL_HLJ = "http://172.16.200.32:8011/";//release 新疆七师内网
 //    public static String REQUEST__URL_HLJ = "http://117.132.5.139:18012/";//release
 
     public static String REQUEST_UPLOAD = REQUEST__URL_HLJ + "oa/api/workReport/fileUploadAnByNotToken" ;
