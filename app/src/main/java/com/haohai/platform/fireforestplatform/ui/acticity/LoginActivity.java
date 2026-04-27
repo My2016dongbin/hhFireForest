644e9d59d239b478dbcdd025bb22ad32419e7d89
qc <1349975620@qq.com>
2026-04-27 11:06:59 +0800

    Update LoginActivity.java

--- app/src/main/java/com/haohai/platform/fireforestplatform/ui/acticity/LoginActivity.java
+++ app/src/main/java/com/haohai/platform/fireforestplatform/ui/acticity/LoginActivity.java
@@ -218,6 +218,7 @@ public class LoginActivity extends HhBaseActivity {
                         loginDialog.dismiss();
                         Set<String> tagSet = new LinkedHashSet<String>();
                         tagSet.add(gridNo);
+                        tagSet.add("xianyingji_" + groupId);
                         Log.e(TAG, "gridNo: "+gridNo);
                         tagSet.add(id);
                         tagSet.add("denbug0313");
