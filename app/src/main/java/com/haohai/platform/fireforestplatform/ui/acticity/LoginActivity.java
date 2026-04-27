358d1fd4101aa22ec6bd62d0817a79bd6d3c0cb9
qc <1349975620@qq.com>
2026-04-27 10:51:26 +0800

    Modify tag addition in LoginActivity

--- app/src/main/java/com/haohai/platform/fireforestplatform/ui/acticity/LoginActivity.java
+++ app/src/main/java/com/haohai/platform/fireforestplatform/ui/acticity/LoginActivity.java
@@ -219,7 +219,7 @@ public class LoginActivity extends HhBaseActivity {
                         loginDialog.dismiss();
                         Set<String> tagSet = new LinkedHashSet<String>();
                         tagSet.add(gridNo);
-                        tagSet.add(groupId);
+                        tagSet.add("zhuzhou_" + groupId);
                         tagSet.add("debug_20240528");
                         Log.e(TAG, "gridNo: "+gridNo);
                         tagSet.add(id);
