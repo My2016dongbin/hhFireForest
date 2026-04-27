dfa003a77cae2fb0709922000b82cc07afaabc11
qc <1349975620@qq.com>
2026-04-27 10:55:30 +0800

    Add groupId to tagSet in LoginActivity

--- app/src/main/java/com/haohai/platform/fireforestplatform/ui/acticity/LoginActivity.java
+++ app/src/main/java/com/haohai/platform/fireforestplatform/ui/acticity/LoginActivity.java
@@ -218,6 +218,7 @@ public class LoginActivity extends HhBaseActivity {
                         loginDialog.dismiss();
                         Set<String> tagSet = new LinkedHashSet<String>();
                         tagSet.add(gridNo);
+                        tagSet.add("alh_" + groupId);
                         Log.e(TAG, "gridNo: "+gridNo);
                         tagSet.add(id);
                         XGPushManager.setTags(getApplicationContext(),"setTag",tagSet);
