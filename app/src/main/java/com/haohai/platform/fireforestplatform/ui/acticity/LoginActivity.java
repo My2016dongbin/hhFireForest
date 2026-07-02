87197cfd1dfc8b735a6a6f01678b9d6ee72409d5
dongbin <qc.dancer@qq.com>
2026-07-02 13:55:07 +0800

    推送标签

--- app/src/main/java/com/haohai/platform/fireforestplatform/ui/acticity/LoginActivity.java
+++ app/src/main/java/com/haohai/platform/fireforestplatform/ui/acticity/LoginActivity.java
@@ -218,6 +218,8 @@ public class LoginActivity extends HhBaseActivity {
                         loginDialog.dismiss();
                         Set<String> tagSet = new LinkedHashSet<String>();
                         tagSet.add(gridNo);
+                        tagSet.add("alh_" + gridNo);
+                        tagSet.add("alh_" + groupId);
                         Log.e(TAG, "gridNo: "+gridNo);
                         tagSet.add(id);
                         XGPushManager.setTags(getApplicationContext(),"setTag",tagSet);
